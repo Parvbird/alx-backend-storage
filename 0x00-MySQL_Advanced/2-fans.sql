3e8824167e31b3fbee7a0ee0e64441faf1994703
Joshua Ajayi <kenduajayi@gmail.com>
2024-01-19 01:27:29 +0000

    task 3

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,4 @@
+-- script which ranks country origins of bands, ordered
+-- by number of "non-unique" fans
+SELECT origin, SUM(fans) as nb_fans from metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
