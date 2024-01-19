de38861c8a6ef9f284490a088245893f29e79a26
Joshua Ajayi <kenduajayi@gmail.com>
2024-01-19 01:37:18 +0000

    task 11

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,19 @@
+-- Script which creates function SafeDiv divides
+-- & retur first by second number or returns
+-- 0 if second number is equal to 0.
+DELIMITER $$ ;
+CREATE FUNCTION SafeDiv(
+	a INT,
+	b INT
+)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+	DECLARE result FLOAT;
+	IF b = 0 THEN
+		RETURN 0;
+        END IF;
+        SET result = (a * 1.0) / b;
+        RETURN result;
+END;$$
+DELIMITER ;
