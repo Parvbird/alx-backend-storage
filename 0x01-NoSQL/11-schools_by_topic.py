61ca4220ead783596754c09705f15ff3d2cddb2d
Joshua <kenduajayi@gmail.com>
2024-01-23 10:35:42 -0800

    task 12

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+find by the topic
+"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Find by the topic
+    """
+    return mongo_collection.find({"topics": topic})
